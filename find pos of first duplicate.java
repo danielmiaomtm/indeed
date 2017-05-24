5d928dab7856dc423a37959f549ec9ce48133994
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-05-24 14:27:13 -0700

    Update find pos of first duplicate.java

--- find pos of first duplicate.java
+++ find pos of first duplicate.java
@@ -1,7 +1,24 @@
+// [java, python, python, java]  第一个出现的重复单词 return java
+
+public String findFirstDuplicate (String[] words) {
+	Set<String> visited = new HashSet<.();
+	for (int i = 0; i < words.length; i++) {
+		if (!visited.contains(words[i])) {
+			visited.add(words[i]);
+		} else {
+			return i;
+		}
+	}
+	return -1;
+}
+
+
+// [java, python, python, java]  最早出现的重复单词 return java
+
 public String findFirstDuplicate (String[] words) {
 		
 		Map<String, Integer> map = new HashMap<>();
-		int minVal = words.length;
+		int minVal = words.length + 1;
 		for (int i = 0; i < words.length; i++) {
 			if (!map.containsKey(words[i])) {
 				map.put(words[i], i);
@@ -9,5 +26,5 @@ public String findFirstDuplicate (String[] words) {
 				minVal = Math.min(minVal, map.get(words[i]));
 			}
 		}
-		return words[minVal];
+		return minVal == words.length + 1 ? "" : words[minVal];
 	}
