d1553830ce06d0e5f29893b7c8262dd4cc97bc49
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-05-23 23:03:37 -0700

    Update expired map.java

--- expired map.java
+++ expired map.java
@@ -42,3 +42,81 @@ class ExpiredMap {
 	//get(k) -> v (less than 1000ms has passed since put)
 	//get(k) -> null (more than 1000ms has passed since put)
 }
+
+
+
+
+
+
+
+//LRU思想
+
+
+class Node<K,V> {
+	Node pre;
+	Node next;
+	V val;
+	K key;
+	long time;
+	Node (K key, V val, long time) {
+		this.key = key;
+		this.val = val;
+		this.pre = null;
+		this.next = null;
+		this.time = time;
+	}
+}
+
+
+public class ExpiredMap<K, V> {
+	Map<K, Node> map = new HashMap<>();
+	Node head =  new Node(0, null, null, 0)
+	Node tail = new Node(0, null, null, 0);
+
+	public void put (K key, V val, long duration) {
+		long curTime = System.currentTimeMillis();
+
+		checkExpired(head, curTime);
+
+		Node temp = new Node(k, val, curTime + duration);
+		if (!map.containsKey(key)) {
+			//x-y-tail
+			moveToTail(temp, tail);
+		} else {
+			node.pre.next = node.next;
+			node.next.pre = node.pre;
+			moveToTail(temp, tail);
+		}
+
+		map.put(key, temp);
+
+	}
+
+
+	public void moveToTail (Node node, Node tail) {
+		tail.pre.next = temp;
+		temp.next = tail;
+		temp.pre = tail.pre;
+		tail.pre = temp;
+	}
+
+
+	public V get (K key) {		
+		long curTime = System.currentTimeMillis();
+
+		checkExpired(head, curTime);
+		return map.containsKey(key) ? map.get(key).val : null;
+	}
+
+	public void checkExpired (Node head, long curTime) {
+		Node node = head.next;
+		while (node != tail && node.time < curTime) {
+			map.remove(node.key);
+			head.next = node.next;
+			node.next.pre = head;
+			node.pre = null;
+			node.next = null;
+			node = head.next;
+		}
+	}
+}
